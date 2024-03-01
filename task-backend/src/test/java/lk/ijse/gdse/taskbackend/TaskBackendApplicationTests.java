d04abee775896fbce776c76c492d019e319baf26
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-03-01 11:01:07 +0530

    init backend

---
+++ task-backend/src/test/java/lk/ijse/gdse/taskbackend/TaskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.taskbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
