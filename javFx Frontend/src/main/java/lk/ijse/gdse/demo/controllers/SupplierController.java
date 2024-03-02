32c3a4accf341d34470f3ee1f34b0d8cbaf60cb1
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-03-02 20:03:03 +0530

    create supplier.fxml

---
+++ javFx Frontend/src/main/java/lk/ijse/gdse/demo/controllers/SupplierController.java
@@ -0,0 +1,59 @@
+package lk.ijse.gdse.demo.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class SupplierController {
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colCode;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colStatus;
+
+    @FXML
+    private TableView<?> tblView;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtCode;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtStatus;
+
+    @FXML
+    void btnDelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSave(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdate(ActionEvent event) {
+
+    }
+
+}
