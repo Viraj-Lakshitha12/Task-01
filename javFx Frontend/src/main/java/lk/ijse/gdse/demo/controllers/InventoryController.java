f1ceba4636c81bbdd95e5e797698329622f3d8a7
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-03-04 23:01:33 +0530

    update navigation

---
+++ javFx Frontend/src/main/java/lk/ijse/gdse/demo/controllers/InventoryController.java
@@ -0,0 +1,101 @@
+package lk.ijse.gdse.demo.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import lk.ijse.gdse.demo.util.Navigation;
+import lk.ijse.gdse.demo.util.Routes;
+
+import java.io.IOException;
+
+public class InventoryController {
+
+    @FXML
+    private ComboBox<?> cmbApprovalStatus;
+
+    @FXML
+    private ComboBox<?> cmbItemId;
+
+    @FXML
+    private ComboBox<?> cmbStatus;
+
+    @FXML
+    private TableColumn<?, ?> colCategory;
+
+    @FXML
+    private TableColumn<?, ?> colCode;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colStatus;
+
+    @FXML
+    private TableColumn<?, ?> colUnit;
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private TableView<?> tblView;
+
+    @FXML
+    private TextField txtDate;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtQty;
+
+    @FXML
+    void btnCategory(ActionEvent event) throws IOException {
+        Navigation.navigate(Routes.CATEGORY, pane);
+
+    }
+
+    @FXML
+    void btnDelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavigateInventrory(ActionEvent event) throws IOException {
+        Navigation.navigate(Routes.INVENTORY, pane);
+
+    }
+
+    @FXML
+    void btnNavigationItem(ActionEvent event) throws IOException {
+        Navigation.navigate(Routes.ITEM, pane);
+    }
+
+    @FXML
+    void btnSave(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplier(ActionEvent event) throws IOException {
+        Navigation.navigate(Routes.SUPPLIER, pane);
+    }
+
+    @FXML
+    void btnUnit(ActionEvent event) throws IOException {
+        Navigation.navigate(Routes.UNIT, pane);
+    }
+
+    @FXML
+    void btnUpdate(ActionEvent event) {
+
+    }
+
+}
