d994688963c67108dc4bc1c1a98390e859b03bea
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-03-04 13:35:20 +0530

    item controller

---
+++ task-backend/src/main/java/lk/ijse/gdse/taskbackend/entity/Item.java
@@ -0,0 +1,28 @@
+package lk.ijse.gdse.taskbackend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Item {
+    @Id
+    private String id;
+    private String code;
+    private String name;
+    private String category;
+    private String unit;
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.Active;
+    public enum Status {
+        Active,
+        Inactive
+    }
+}
