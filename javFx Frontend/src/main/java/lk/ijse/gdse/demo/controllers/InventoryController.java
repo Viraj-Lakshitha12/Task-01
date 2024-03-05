fc720ce71b25f5061fe8f821c7ce1885d3e16cdd
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-03-05 10:21:14 +0530

    update inventory form

--- javFx Frontend/src/main/java/lk/ijse/gdse/demo/controllers/InventoryController.java
+++ javFx Frontend/src/main/java/lk/ijse/gdse/demo/controllers/InventoryController.java
@@ -1,26 +1,33 @@
 package lk.ijse.gdse.demo.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
-import lk.ijse.gdse.demo.util.Navigation;
-import lk.ijse.gdse.demo.util.Routes;
+import lk.ijse.gdse.demo.dto.Item;
 
-import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class InventoryController {
-    @FXML
-    private ComboBox<?> cmbApprovalStatus;
 
     @FXML
-    private ComboBox<?> cmbItemId;
+    private ComboBox<String> cmbApprovalStatus;
 
     @FXML
-    private ComboBox<?> cmbStatus;
+    private ComboBox<String> cmbItemId;
+
+    @FXML
+    private ComboBox<String> cmbStatus;
 
     @FXML
     private TableColumn<?, ?> colApproval_status;
@@ -32,7 +39,22 @@ public class InventoryController {
     private TableColumn<?, ?> colId;
 
     @FXML
+    private TableColumn<?, ?> colItemCategory;
+
+    @FXML
+    private TableColumn<?, ?> colItemCode;
+
+    @FXML
     private TableColumn<?, ?> colItemId;
+
+    @FXML
+    private TableColumn<?, ?> colItemName;
+
+    @FXML
+    private TableColumn<?, ?> colItemStatus;
+
+    @FXML
+    private TableColumn<?, ?> colItemUnit;
 
     @FXML
     private TableColumn<?, ?> colQty;
@@ -47,7 +69,7 @@ public class InventoryController {
     private TableView<?> tblView;
 
     @FXML
-    private TextField txtDate;
+    private TableView<Item> tblViewItems;
 
     @FXML
     private TextField txtId;
@@ -55,9 +77,45 @@ public class InventoryController {
     @FXML
     private TextField txtQty;
 
+    public void initialize(){
+        ObservableList<String> statusList = FXCollections.observableArrayList("Active", "Inactive");
+        cmbStatus.setItems(statusList);
+        ObservableList<String> list = FXCollections.observableArrayList("Pending", "Approved");
+        cmbApprovalStatus.setItems(list);
+        setCellValueFactory();
+        cmbItemId.setItems(fetchDataForComboBox("http://localhost:8080/api/items/getIds"));
+    }
+    private void setCellValueFactory() {
+        colItemId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colItemName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colItemCode.setCellValueFactory(new PropertyValueFactory<>("code"));
+        colItemCategory.setCellValueFactory(new PropertyValueFactory<>("category"));
+        colItemUnit.setCellValueFactory(new PropertyValueFactory<>("unit"));
+        colItemStatus.setCellValueFactory(new PropertyValueFactory<>("status"));
+    }
+    private ObservableList<String> fetchDataForComboBox(String url) {
+        try {
+            HttpClient httpClient = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String[] data = objectMapper.readValue(response.body(), String[].class);
+            return FXCollections.observableArrayList(data);
+        } catch (Exception e) {
+            System.out.println("Error occurred: " + e.getMessage());
+            return FXCollections.observableArrayList();
+        }
+    }
+
+
     @FXML
-    void btnCategory(ActionEvent event) throws IOException {
-        Navigation.navigate(Routes.CATEGORY, pane);
+    void btnCategory(ActionEvent event) {
 
     }
 
@@ -67,14 +125,13 @@ public class InventoryController {
     }
 
     @FXML
-    void btnNavigateInventrory(ActionEvent event) throws IOException {
-        Navigation.navigate(Routes.INVENTORY, pane);
+    void btnNavigateInventrory(ActionEvent event) {
 
     }
 
     @FXML
-    void btnNavigationItem(ActionEvent event) throws IOException {
-        Navigation.navigate(Routes.ITEM, pane);
+    void btnNavigationItem(ActionEvent event) {
+
     }
 
     @FXML
@@ -83,18 +140,45 @@ public class InventoryController {
     }
 
     @FXML
-    void btnSupplier(ActionEvent event) throws IOException {
-        Navigation.navigate(Routes.SUPPLIER, pane);
+    void btnSupplier(ActionEvent event) {
+
     }
 
     @FXML
-    void btnUnit(ActionEvent event) throws IOException {
-        Navigation.navigate(Routes.UNIT, pane);
+    void btnUnit(ActionEvent event) {
+
     }
 
     @FXML
     void btnUpdate(ActionEvent event) {
 
     }
+    private Item fetchItemData(String url) {
+        try {
+            HttpClient httpClient = HttpClient.newHttpClient();
 
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            Item data = objectMapper.readValue(response.body(), Item.class);
+            ObservableList<Item> itemList = FXCollections.observableArrayList(data);
+
+            tblViewItems.setItems(itemList);
+            return data;
+
+        } catch (Exception e) {
+            System.out.println("Error occurred: " + e.getMessage());
+        }
+        return null;
+    }
+
+    public Item cmbOnActionItemId(ActionEvent actionEvent) {
+        String cmbUnitValue = cmbItemId.getValue();
+        return fetchItemData("http://localhost:8080/api/unit/getUnitById/" + cmbUnitValue);
+    }
 }
